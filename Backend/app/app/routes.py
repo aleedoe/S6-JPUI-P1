4fea7e3c1b3933a19dd5d53fcc1cabbd5ce321ce
alie <mushyafaali@gmail.com>
2025-03-27 11:11:48 +0700

    edit endpoint

--- Backend/app/app/routes.py
+++ Backend/app/app/routes.py
@@ -5,6 +5,6 @@ user_bp = Blueprint('user_bp', __name__)
 def register_blueprint_user(app):
     app.register_blueprint(user_bp)
 
-@user_bp.route('/home/', methods=['GET'])
+@user_bp.route('/', methods=['GET'])
 def home():
     return "Halo, Selamat datang di aplikasi Flask!"
